a8a63342e0fa328a1f010f13ac8b76da618c631c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-05 21:27:45 +0300

    Create Course3-0004-IntegerDataTypes.rs

---
+++ Course3-0004-IntegerDataTypes.rs
@@ -0,0 +1,13 @@
+fn main() {
+
+    // u -> from 8 to 128, non-negative or zero
+    // i -> from 8 to 128, all 
+
+    let x: i8 = 8;
+
+    let y: u128 = 99999999999999;
+
+    let z: i16 = -550;
+    
+}
+
