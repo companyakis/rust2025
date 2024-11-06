4384d29618157ce6f98781b8a0ed7ab1af413057
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-06 22:47:15 +0300

    Create Course3-0009-Boolean-data-type-and-operations.rs

---
+++ Course3-0009-Boolean-data-type-and-operations.rs
@@ -0,0 +1,17 @@
+fn main() {
+
+  let x: bool = true;
+
+  let y = false;
+
+  println!("Not operator => {}", !x);
+
+  println!("And operator => {}", x & y);
+
+  println!("Or operator => {}", x | y)
+
+}
+
+// Not operator => false
+// And operator => false
+// Or operator => true  
