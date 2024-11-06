b45b9e6680773349575e03aca76be4f7e50f63c7
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-06 22:55:41 +0300

    Create Course3-0010-Comparison-operations.rs

---
+++ Course3-0010-Comparison-operations.rs
@@ -0,0 +1,21 @@
+fn main() {
+
+  let x: u8 = 5;
+
+  let y: u8 = 3;
+
+  println!("{}", x > y);
+  println!("{}", x >= y);
+  println!("{}", x < y);
+  println!("{}", x <= y);
+  println!("{}", x == y);
+  println!("{}", x != y)
+}
+
+// true
+// true 
+// false
+// false
+// false
+// true 
+
