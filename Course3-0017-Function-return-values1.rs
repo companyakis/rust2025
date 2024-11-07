0d6d9e401f1f5bda7dd0b677558645fd04aa1408
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-07 22:11:02 +0300

    Create Course3-0017-Function-return-values1.rs

---
+++ Course3-0017-Function-return-values1.rs
@@ -0,0 +1,16 @@
+fn main() {
+
+  let result = sum(-8988, 2198844);
+
+  println!("Sum = {}", result) // Sum = 2189856
+
+}
+
+fn sum(x: i128, y: i128) -> i128 {
+
+  let sum = x + y;
+
+  return sum;
+}
+
+
