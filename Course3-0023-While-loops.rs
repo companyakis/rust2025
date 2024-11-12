0c6dc05b763831f4b90c760e433e48bfc6525aad
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-12 10:17:54 +0300

    Create Course3-0023-While-loops.rs

---
+++ Course3-0023-While-loops.rs
@@ -0,0 +1,23 @@
+fn main() {
+
+  let mut index= 0;
+
+  let years = [2000, 2004, 2007, 2015, 2024];
+
+  while index < years.len() {
+
+    println!("Year: {}", years[index]);
+
+    index += 1
+  } 
+}
+
+// Year: 2000
+// Year: 2004
+// Year: 2007
+// Year: 2015
+// Year: 2024
+
+
+
+
