e11b2e319db48192340fe5b4d2ce14c21ca1a002
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-12 09:47:08 +0300

    Create Course3-0020-Multiple-conditions.rs

---
+++ Course3-0020-Multiple-conditions.rs
@@ -0,0 +1,26 @@
+fn main() {
+
+  let x: i16 = -2024;
+
+  let y: u16 = 2027;
+
+  if x > y as i16 {
+
+    println!("x > y")
+  }
+
+  else if y as i16 > x {
+
+    println!("y > x")
+  }
+
+  else {
+
+    println!("x = y")
+  }
+
+}
+
+
+
+
