0b10201f039f3e5056f36e1b45e62f870963ad53
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-12 09:51:31 +0300

    Create Course3-0021-Conditional-assignment1.rs

---
+++ Course3-0021-Conditional-assignment1.rs
@@ -0,0 +1,20 @@
+fn main() {
+
+  let completed: bool = false;
+
+  let salary: u16;
+
+  if completed {
+    salary = 6500
+  }
+  else {
+      salary = 3000;
+  }
+
+  println!("Salary: {salary}")
+  
+}
+
+
+
+
