cae4ab3ab763d2bf6ebb3065dcb9e5fe1610fadb
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-13 17:55:47 +0300

    Create Course3-0030-Moving-cloning-and-copying-data2.rs

---
+++ Course3-0030-Moving-cloning-and-copying-data2.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+  // heap data!
+
+  let greeting = String::from("Hi there!");
+
+  let say_hi = greeting.clone();
+
+  println!("{greeting}"); // Hi there!
+
+  println!("{say_hi}"); // Hi there!
+
+}
+
